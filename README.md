8865545f678313c39b41162461f81feb46683596
Brian Turner <bturner@texascdlprep.com>
2021-12-11 13:54:37 -0600

    commit update

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Team_Profile_Generator
+
 A Node.js command-line application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person.
+
+# Good_README_Generator <div align="right">[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)</div>
+
+## Description
+
+> This is a github project repository.
+
+## Table of Contents
+
+1. [Installation](#install)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+## Installation <a name="install"></a>
+
+You will only need an active browser to view this portfolio correctly. No installations requried! :+1:
+
+## Usage <a name="usage"></a>
+
+No specifics.
+
+## License <a name="license"></a>
+
+This project is Apache licensed.
+
+## Contributing <a name="contributing"></a>
+
+Feel free to fork this repository, and open a pull request to suggest changes. Please provide a short note on your updates and/or additions!
+
+## Tests <a name="tests"></a>
+
+None
+
+## Questions <a name="questions"></a>
+
+For any additional questions or other interests in this project, please contact me at:  
+ github.com/hihellos  
+ or  
+ hihellos@github.com  
+ :woman_technologist:
--- index.js
+++ index.js
@@ -1,74 +1,122 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+// import { password, prompter, text, yesno } from "@gweiermann/inquirer-builder";
+
+
+inquirer.prompt([{
+  name: 'movie',
+  type: 'list',
+  choices: ['test1','text2','test3']
+}, {
+  when: function (response) {
+    return response.('test1');
+  },
+  name: 'good-or-not',
+  message: 'Sweet! Was it any good?'
+}], function (response) {});
+
+
 
 // Use writeFileSync method to use promises instead of a callback function
 
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    },
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
-    },
-    {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
-    },
-    {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
-    },
-  ]);
-};
+// let strategies = [];
+// let repeat = true;
+// do {
+//   strategies.push(
+//      inquirer.prompt([
+//       {
+//         type: "input",
+//         name: "firstname",
+//         message: "Enter firstname",
+//       },
+//       {
+//         type: "input",
+//         name: "lastname",
+//         message: "Enter lastname",
+//       },
+//     ])
+//   );
+//   repeat = (
+//      inquirer.prompt([
+//       {
+//         type: "confirm",
+//         name: "repeat",
+//         message: "Do you want to add another person ?",
+//       },
+//     ])
+//   ).repeat;
+// } while (repeat);
 
-const generateHTML = ({ name, location, github, linkedin }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${name}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+// console.log(strategies);
 
-// Bonus using writeFileSync as a promise
-const init = () => {
-  promptUser()
-    // Use writeFileSync method to use promises instead of a callback function
-    .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
-    .then(() => console.log('Successfully wrote to index.html'))
-    .catch((err) => console.error(err));
-};
 
-init();
+
+// const promptUser = () => {
+//   return inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your name?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'location',
+//       message: 'Where are you from?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'hobby',
+//       message: 'What is your favorite hobby?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'food',
+//       message: 'What is your favorite food?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'github',
+//       message: 'Enter your GitHub Username',
+//     },
+//     {
+//       type: 'input',
+//       name: 'linkedin',
+//       message: 'Enter your LinkedIn URL.',
+//     },
+//   ]);
+// };
+
+// const generateHTML = ({ name, location, github, linkedin }) =>
+//   `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <div class="jumbotron jumbotron-fluid">
+//   <div class="container">
+//     <h1 class="display-4">Hi! My name is ${name}</h1>
+//     <p class="lead">I am from ${location}.</p>
+//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+//     <ul class="list-group">
+//       <li class="list-group-item">My GitHub username is ${github}</li>
+//       <li class="list-group-item">LinkedIn: ${linkedin}</li>
+//     </ul>
+//   </div>
+// </div>
+// </body>
+// </html>`;
+
+// // Bonus using writeFileSync as a promise
+// const init = () => {
+//   promptUser()
+//     // Use writeFileSync method to use promises instead of a callback function
+//     .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
+//     .then(() => console.log('Successfully wrote to index.html'))
+//     .catch((err) => console.error(err));
+// };
+
+// // init();
